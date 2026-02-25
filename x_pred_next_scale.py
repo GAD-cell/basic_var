d294af514b6590067dc4c55d63645757fd6402a3
Edgar Duc <edgar.duc@eleves.enpc.fr>
2026-02-25 21:38:52 +0100

    Kaggle parameters

--- x_pred_next_scale.py
+++ x_pred_next_scale.py
@@ -436,7 +436,7 @@ if __name__ == "__main__":
         real_features_path="data/cifar10_train_dinov2_features.pt", 
         real_subset=50000, knn_k=3, use_amp=_use_amp(device), 
         device=device.type, 
-        use_wandb=False,
+        use_wandb=True,
         wandb_run_name="cifar10-var-L4-H4-d128-e1000",
         ckpt_every_n_steps=40_000,
     )
@@ -458,4 +458,4 @@ if __name__ == "__main__":
     )
 
     model = XPredNextScale(cfg)
-    train_cifar10(model, train_cfg, data_root="data", feature_split="train", feature_batch_size=128, force_recompute_features=False, progress_bar=True)
+    train_cifar10(model, train_cfg, data_root="data", feature_split="train", feature_batch_size=128, force_recompute_features=False, progress_bar=False)
